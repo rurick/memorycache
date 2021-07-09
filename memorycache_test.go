07a52e97e9c82fdb2f0d8ec9608c87cd36ee0f1e
Юрий Иовков <y.iovkov@digital-kitchen.ru>
2021-07-09 10:31:57 +0300

    add test for KeyGen func

--- memorycache_test.go
+++ memorycache_test.go
@@ -11,6 +11,44 @@ const (
 	testValue    string = "Hello world"
 )
 
+func TestKeyGen(t *testing.T) {
+	tests := []struct {
+		name string
+		args interface{}
+		want string
+	}{
+		{
+			"strings in key",
+			[]string{"k1", "k2", "k3"},
+			"eb98f46aca624b1e402947677c54d025dc463a67",
+		}, {
+			"int64 in key",
+			[]int64{1, 2, 3},
+			"6d780b01458b623aa5f77db71ac9a02ff1d5ecda",
+		}, {
+			"mixed key",
+			[]interface{}{1, "k2", 3},
+			"ebdf5f5fd2817d5534d668548298bd135ae3dd0e",
+		}, {
+			"mixed key",
+			[]interface{}{1, "k2", 3, map[int]string{1: "a"}},
+			"60b428a15bf1e9eca224ef24e37c38ec8d8f86f9",
+		}, {
+			"mixed key with nil",
+			[]interface{}{1, "k2", nil, map[int]string{1: "a"}},
+			"3b381ec1e7defe06ddf6a4eeb062211c579888bf",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			k := KeyGen(tt.args)
+			if k != tt.want {
+				t.Errorf("KeyGen(%v), want %v, got %v ", tt.args, tt.want, k)
+			}
+		})
+	}
+}
+
 // Test_Get get cache by key
 func Test_Get(t *testing.T) {
 	cache := New(10*time.Minute, 1*time.Hour)
